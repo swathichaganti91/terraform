8fd08ecb9a0bbba3fb6bbe66d08d8df54a8ab634
root <root@ip-172-31-46-197.ap-south-1.compute.internal>
2025-08-09 06:00:46 +0000

    day-4 lockfile&dynamoDB

--- day-4 lockfile&dynamoDB/main.tf
+++ day-4 lockfile&dynamoDB/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_instance" "name" {
   ami           = "ami-0d0ad8bb301edb745"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
 
   tags = {
     Name = "dev"
