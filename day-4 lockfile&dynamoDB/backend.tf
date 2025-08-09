1474cedd991d64558b731cec258409dca94c730f
swathichaganti91 <suseelalucky227@gmail.com>
2025-08-09 12:43:59 +0530

    day-4

--- day-4 lockfile&dynamoDB/backend.tf
+++ day-4 lockfile&dynamoDB/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "theswathi.shop"
     key    = "terraform.tfstate"
     region = "ap-south-1"
-    #use_lockfile = true #s3 supports this feature but teraaform version > 1.10, latest version >=1.10
-    
+    use_lockfile = true #s3 supports this feature but teraaform version > 1.10, latest version >=1.10
+    dynamodb_table = "test"
   }
 }
--- day-4 lockfile&dynamoDB/main.tf
+++ day-4 lockfile&dynamoDB/main.tf
@@ -5,10 +5,10 @@ provider "aws" {
 
 resource "aws_instance" "name" {
   ami           = "ami-0d0ad8bb301edb745"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
 
   tags = {
-    Name = "dev"
+    Name = "tedhbcdehvtv"
   }
 }
 
