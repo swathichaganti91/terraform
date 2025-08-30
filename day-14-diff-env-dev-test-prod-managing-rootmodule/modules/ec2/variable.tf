3e1cabe85bad980f11b0dc11e7ce46c7878fc106
swathichaganti91 <suseelalucky227@gmail.com>
2025-08-30 23:43:49 +0530

    day-14

---
+++ day-14-diff-env-dev-test-prod-managing-rootmodule/modules/ec2/variable.tf
@@ -0,0 +1,21 @@
+variable "ami_id" {
+    type = string
+    default = ""
+  
+}
+variable "instance_type" {
+    type = string
+    default = ""
+  
+}
+variable "subnet_id" {
+  
+}
+variable "env" {
+  
+}
+variable "Key_name" {
+    type = string
+    default = ""
+  
+}
