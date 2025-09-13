a369b02b3e5ead8c22969b665f907ba47737aed8
swathichaganti91 <suseelalucky227@gmail.com>
2025-09-13 13:00:01 +0530

    Updated ASG, launch templates, and backend port to 80

--- day-14-3tier-project/modules/compute/main.tf
+++ day-14-3tier-project/modules/compute/main.tf
@@ -1,16 +1,22 @@
 resource "aws_launch_template" "backend_lt" {
     name_prefix = "lt"
-    image_id = var.ami_id
-    instance_type = var.instance_type
+    image_id = var.backend_ami_id
+    instance_type = "t3.small"
 
     user_data = base64encode(<<-EOF
                 #!/bin/bash
                 yum update -y
-                yum install git -y
+                yum install git -y git nodejs npm
+                npm install -g pm2
                 cd /root
                 if [ ! -d "2nd10WeeksofCloudOps-main" ]; then
-                git clone https://github.com/<your-repo>/2nd10WeeksofCloudOps-main.git
-                cd repo/backend
+                git clone https://github.com/CloudTechDevOps/2nd10WeeksofCloudOps-main.git
+                fi
+
+                cd 2nd10WeeksofCloudOps-main/backend || {
+                echo "Failed to change directory to backend. Exiting."
+                exit 1
+}
 
                 echo "DB_HOST=${module.rds.db_endpoint}" >>.env
                 echo "DB_USER=${module.rds.db_user}" >> .env
@@ -30,76 +36,48 @@ resource "aws_launch_template" "backend_lt" {
                 }
   
 }
+###target group backend
 resource "aws_lb_target_group" "backend_tg" {
     name = "backend-tg"
-    port = 3000
+    port = 80
     protocol = "HTTP"
     vpc_id = module.vpc.vpc_id
   
 }
+###backend ASG
 resource "aws_autoscaling_group" "backend_asg" {
     desired_capacity = 1
-    max_size = 3
+    max_size = 1
     min_size = 1
     vpc_zone_identifier = [module.vpc.private_subnet_ids[7],
                            module.vpc.private_subnet_ids[8]
     ]
     launch_template {
       id = aws_launch_template.backend_lt.id
-      version = "$latest"
+      version = "Latest"
     }
     target_group_arns = [aws_lb_target_group.backend_tg.arn]
   
 }
-resource "aws_launch_template" "frontend_lt" {
-    name_prefix = "frontend-lt"
-    image_id = "ami-0861f4e788f5069dd"
-    instance_type = var.instance_type
-
-    user_data = base64encode(<<-EOF
-                #!/bin/bash
-                yum update -y
-                yum install -y git httpd nodejs npm
-                
-                cd/home/ec2-user
-                git clone https://github.com/your/repo.git
-                cd repo/client
-                
-                echo "const backendUrl='http://${aws_lb.app_alb.dns.name}'" > src/pages/config.js
-                
-                npm install
-                npm run build
-                
-                cp -r build/* /var/www/html
-                systemctl enable httpd
-                syatemctl start httpd
-                EOF
-                )
-
-                network_interfaces {
-                  associate_public_ip_address = true
-                  security_groups = [module.vpc.sg_id]
-                }
-  
-}
-
-
 #######frontend launch template#####
 
 resource "aws_launch_template" "frontend_lt" {
     name_prefix = "frontend-lt"
-    image_id = "ami-0861f4e788f5069dd"
-    instance_type = var.instance_type
+    image_id = var.frontend_ami_id
+    instance_type = "t3.small"
 
     user_data = base64encode(<<-EOF
                 #!/bin/bash
+                
                 yum install -y git httpd nodejs npm
                 
-                cd /home/ec2-user
-                git clone https://github.com/your/repo.git
-                cd repo/client
                 
-                echo "cons backendUrl='http://${aws_lb.app_alb.dns_name}'" > srd/pages/config.js
+                cd /home/ec2-user
+                git clone https://github.com/CloudTechDevOps/2nd10WeeksofCloudOps-main.git
+                cd 2nd10WeeksofCloudOps-main/client
+
+                
+                echo "const backendUrl='http://${aws_lb.app_alb.dns_name}/api'" > src/pages/config.js
                 
                 npm install
                 npm run build
@@ -117,25 +95,18 @@ resource "aws_launch_template" "frontend_lt" {
 
   
 }
-resource "aws_lb_target_group" "fontend_tg" {
+#frontend targetgroup
+resource "aws_lb_target_group" "frontend_tg" {
     name = "frontend-tg"
     port = 80
     protocol = "HTTP"
     vpc_id = module.vpc.vpc_id
   
 }
-
-resource "aws_lb_target_group" "frontend_tg" {
-    name = "frontend-tg"
-    port = 80
-    protocol = "http"
-    vpc_id = module.vpc.vpc_id
-  
-}
-
+##frontend ASG
 resource "aws_autoscaling_group" "frontend_asg" {
     desired_capacity = 1
-    max_size = 2
+    max_size = 1
     min_size = 1
     vpc_zone_identifier = [module.vpc.private_subnet_ids[5], module.vpc.private_subnet_ids[6]]
 
@@ -144,7 +115,7 @@ resource "aws_autoscaling_group" "frontend_asg" {
       version = "$Latest"
 
     }
-    target_group_arns = [aws_lb_target_group.fontend_tg.arn]
+    target_group_arns = [aws_lb_target_group.frontend_tg.arn]
   
 }
 
@@ -158,15 +129,30 @@ resource "aws_lb" "app_alb" {
     security_groups = [module.vpc.sg_id]
     subnets = module.vpc.public_subnet_ids
 }
-resource "aws_lb_listener_rule" "frontend_rule" {
-    listener_arn = aws_lb_listener.frontend_listener.arn
-    priority = 1
+##listener frotend
+resource "aws_lb_listener" "frontend_listener" {
+    load_balancer_arn = aws_lb.app_alb.arn
+    port = 80
+    protocol = "HTTP"
 
-    action {
+    default_action {
       type = "forward"
       target_group_arn = aws_lb_target_group.frontend_tg.arn
 
     }
+}
+## frontend rule
+resource "aws_lb_listener_rule" "frontend_rule" {
+  listener_arn = aws_lb_listener.frontend_listener.arn
+  priority = 1
+
+  action {
+   type = "forward"
+   target_group_arn = aws_lb_target_group.frontend_tg.arn 
+  }
+  
+
+
     condition {
       path_pattern {
         values = ["/app/*"]
@@ -174,9 +160,10 @@ resource "aws_lb_listener_rule" "frontend_rule" {
     }
   
 }
+##listener backend rule
 resource "aws_lb_listener_rule" "backend_rule" {
-    listener_arn = aws_lb_listener.backend_listener.arn
-    priority = 1
+    listener_arn = aws_lb_listener.frontend_listener.arn
+    priority = 2
 
     action {
       type = "forward"
